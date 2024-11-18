3370de7d80c751b6943b066914e9d81f9432c2a8
ikhwan.mprasojo <ikhwan.mprasojo@gmail.com>
2024-11-18 07:57:35 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from molmass import Formula
+
+# Component
+COMPONENT = {
+    "SULPHUR_DIOXIDE": Formula('SO2'),
+    "CARBON_MONOXIDE": Formula('CO'),
+    "CARBON_DIOXIDE" : Formula('CO2'),
+    "NITROGEN"       : Formula('N2'),
+}
